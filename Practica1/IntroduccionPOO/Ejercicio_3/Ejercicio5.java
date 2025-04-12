76820d1e42720db1117fa56ec81146d1b35e733d
jhennyfloresm <jfloresm2@fcpn.edu.bo>
2025-04-11 20:15:42 -0400

    Add files via upload

---
+++ Practica1/IntroduccionPOO/Ejercicio_3/Ejercicio5.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio5;
+
+/**
+ *
+ * @author WINDOWS 11
+ */
+public class Ejercicio5 {
+ public static void main(String[] args) {
+        Estudiante estudiante1 = new Estudiante("esteban", 9, 6);
+        Estudiante estudiante2 = new Estudiante("dido ", 8, 7);
+        Estudiante estudiante3 = new Estudiante("soyi", 5, 5);
+        estudiante1.mostrar();
+        estudiante2.mostrar();
+       estudiante3.mostrar();
+
+
+        }
+    }
+
+
+
