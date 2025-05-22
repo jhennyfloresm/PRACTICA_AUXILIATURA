77664ce12feb8e7cab7635000eb6fd623a81c404
jhennyfloresm <jfloresm2@fcpn.edu.bo>
2025-05-22 15:36:56 -0400

    Update Prac2_5.java

--- Practica_2/herencia/Ejercicio 5/Prac2_5.java
+++ Practica_2/herencia/Ejercicio 5/Prac2_5.java
@@ -1,11 +1,9 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
-
 package com.mycompany.prac2_5;
 
-import java.util.ArrayList;
+
 
 /**
  *
@@ -14,36 +12,33 @@ import java.util.ArrayList;
 public class Prac2_5 {
 
     public static void main(String[] args) {
-      
-        ArrayList<Gerente> gerentes = new ArrayList<>();
-        gerentes.add(new Gerente("jgawin", " joss", 100, 9, "marketing", 100));
-        gerentes.add(new Gerente("top", "Titr", 800, 4, "cocina", 400));
+        Gerente g1 = new Gerente("jgawin", "joss", 100, 9, "marketing", 100);
+        Gerente g2 = new Gerente("top", "Titr", 800, 4, "cocina", 4000);
 
-        ArrayList<Desarrolador> desarrolladores = new ArrayList<>();
-        desarrolladores.add(new Desarrolador("sofia", "lopes", 2500, 2, "Java", 45));
-        desarrolladores.add(new Desarrolador("pedro", "peres", 4500, 3, "Python",6));
-
+        Desarrolador d1 = new Desarrolador("sofia", "lopes", 2500, 2, "Java", 45);
+        Desarrolador d2 = new Desarrolador("pedro", "peres", 4500, 3, "Python", 6);
+        Desarrolador d3 = new Desarrolador("maria", "gomez", 4200, 5, "C++", 12);
+        //a
         System.out.println("== Salarios ==");
-        for (Gerente g : gerentes) g.mostrar();
-        for (Desarrolador d : desarrolladores) d.mostrar();
+        g1.mostrar();
+        g2.mostrar();
+        d1.mostrar();
+        d2.mostrar();
+        d3.mostrar();
 
-        //inciso v
+        // ejercico
         System.out.println("\n== Gerentes con bono > 1000 ==");
-        for (Gerente g : gerentes) {
-            if (g.getBonoGerencial() > 1000) {
-                g.mostrar();
-            }
-        }
+        if (g1.getBonoGerencial() > 1000) g1.mostrar();
+        if (g2.getBonoGerencial() > 1000) g2.mostrar();
 
-        // d) Desarrolladores con > 10 horas extras
+        // ejercicio
         System.out.println("\n== Desarrolladores con más de 10 horas extras ==");
-        Iterable<Desarrolador> desarroladores = null;
-        for (Desarrolador d : desarroladores) {
-            if (d.getHorasExtras() > 10) {
-                d.mostrar();
-            }
-        }
+        if (d1.getHorasExtras() > 10) d1.mostrar();
+        if (d2.getHorasExtras() > 10) d2.mostrar();
+        if (d3.getHorasExtras() > 10) d3.mostrar();
     }
 }
+
+
 
 
