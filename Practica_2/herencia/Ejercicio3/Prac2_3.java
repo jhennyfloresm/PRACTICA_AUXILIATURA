3e11bece078b217e12c1a993479924d3cf9e0a79
jhennyfloresm <jfloresm2@fcpn.edu.bo>
2025-05-22 15:19:30 -0400

    Update Prac2_3.java

--- Practica_2/herencia/Ejercicio3/Prac2_3.java
+++ Practica_2/herencia/Ejercicio3/Prac2_3.java
@@ -2,45 +2,34 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.prac2_3;
-
-
-/**
- *
- * @author WINDOWS 11
- */
 public class Prac2_3 {
     public static void main(String[] args) {
-        // Estudiantes
-        Estudiante e1 = new Estudiante("11", "Rosa", 2004, "Chavez", "7771", "R11", "12/09/2024", "5");
-        Estudiante e2 = new Estudiante("22", "Paolo", 2000, "Pilar", "7772", "P22", "12/09/2024", "5");
-        Estudiante e3 = new Estudiante("33", "Lucia", 1999, "Torrez", "77773", "L33", "12/09/2024", "5");
+
+        Estudiante e1 = new Estudiante("11", "pedro", 2004, "Chambi", "7771", "R11", "12/09/2024", "5");
+        Estudiante e2 = new Estudiante("22", "juan", 2000, "Plod", "7772", "P22", "12/09/2024", "5");
+ 
         System.out.println("------ Estudiante 1 -------");
         e1.mostrar();
         System.out.println("\n------ Estudiante 2 -------");
         e2.mostrar();
-        System.out.println("\n------ Estudiante 3 -------");
-        e3.mostrar();
 
-        // Docentes
-        Docente d1 = new Docente("456123", "Maria", "Chavez", "79988776", 1987, "NIT321", "Ingeniero", "Femenino");
-        Docente d2 = new Docente("67889985", "Armando", "Chavez", "67889985", 1991, "NIT322", "Ingeniero", "Masculino");
+        Docente d1 = new Docente("456123", "Mocs", "llol", "79988776", 1987, "NIT321", "fisica", "Femenino");
+        Docente d2 = new Docente("67889985", "tito", "Chavez", "67889985", 1991, "NIT322", "ingeniero", "Masculino");
 
         System.out.println("\n------- Docente 1 --------");
         d1.mostrar();
         System.out.println("\n------- Docente 2 --------");
         d2.mostrar();
 
-        // Lógica adicional
         Mayores25(e1);
         Mayores25(e2);
-        Mayores25(e3);
+      
 
         IngenieroMayor(d1);
         IngenieroMayor(d2);
 
         Apellidos(d1, e1);
-        Apellidos(d2, e3);
+        Apellidos(d2, e2);
     }
 
     public static void Mayores25(Estudiante e) {
@@ -54,7 +43,7 @@ public class Prac2_3 {
     }
 
     public static void IngenieroMayor(Docente d) {
-        System.out.println("\nEvaluando docente masculino ingeniero más longevo:");
+        System.out.println("\n  docente ingeniero:");
         if (d.getProfesion().equalsIgnoreCase("Ingeniero") && d.getSexo().equalsIgnoreCase("Masculino")) {
             System.out.println("Docente encontrado:");
             d.mostrar();
@@ -71,5 +60,7 @@ public class Prac2_3 {
         }
     }
 }
+
+
 
 
