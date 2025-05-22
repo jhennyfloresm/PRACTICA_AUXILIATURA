b6d40110fb79a4eb993d2b071311840a91f851ba
jhennyfloresm <jfloresm2@fcpn.edu.bo>
2025-05-22 16:31:46 -0400

    Update Portero.java

--- Practica_2/Composicion/Ejer5/Portero.java
+++ Practica_2/Composicion/Ejer5/Portero.java
@@ -8,7 +8,7 @@ package com.mycompany.prac.com5;
  *
  * @author WINDOWS 11
  */
-    public class Portero extends Jugador {
+     public class Portero extends Jugador {
     private String habilidadEspecial;
 
     public Portero(String nombre, int numero, String habilidadEspecial) {
@@ -16,13 +16,21 @@ package com.mycompany.prac.com5;
         this.habilidadEspecial = habilidadEspecial;
     }
 
+    public String getHabilidadEspecial() {
+        return habilidadEspecial;
+    }
+
+    public void setHabilidadEspecial(String habilidadEspecial) {
+        this.habilidadEspecial = habilidadEspecial;
+    }
+
+    
     @Override
     public void mostrarInfo() {
         super.mostrarInfo();
         System.out.println("Habilidad especial: " + habilidadEspecial);
     }
 }
-
 
 
 
