7c9b3546453a15620d0993bb82dfd6d723d55b80
jhennyfloresm <jfloresm2@fcpn.edu.bo>
2025-05-22 14:00:10 -0400

    Add files via upload

---
+++ Practica_2/Composicion/Ejer5/PracCom5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prac.com5;
+
+/**
+ *
+ * @author WINDOWS 11
+ */
+public class PracCom5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
